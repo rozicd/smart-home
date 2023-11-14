0b54bd69a294baa9cdace078b036ec622ae26f55
ognjen <ognjen34car@gmail.com>
2023-11-14 23:54:16 +0100

    added Basic Radio Buttons

--- frontend/src/Components/Services/useFetch.jsx
+++ frontend/src/Components/Services/useFetch.jsx
@@ -1,34 +1,34 @@
 import { useEffect, useState } from "react";
 
-const useFetch = (url, dependencies = []) => {
-  const [data, setData] = useState({});
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+const useFetch = (callbacks, dependencies = []) => {
+    const [data, setData] = useState({});
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dataPromises = Object.keys(callbacks).map(async (key) => {
-          return { [key]: await callbacks[key]() };
-        });
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const dataPromises = Object.keys(callbacks).map(async (key) => {
+                    return { [key]: await callbacks[key]() };
+                });
 
-        const dataResults = await Promise.all(dataPromises);
+                const dataResults = await Promise.all(dataPromises);
 
-        const mergedData = Object.assign({}, ...dataResults);
+                const mergedData = Object.assign({}, ...dataResults);
 
-        setData(mergedData);
+                setData(mergedData);
+                console.log(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
         console.log(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    console.log(data);
-    fetchData();
-  }, dependencies);
+        fetchData();
+    }, dependencies);
 
-  return { ...data, isLoading, error };
+    return { ...data, isLoading, error };
 };
 
 export default useFetch;
