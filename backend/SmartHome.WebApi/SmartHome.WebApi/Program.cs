dd0d2db385cee58afa7ab07d9a488db7bd44b31d
MarkoErdelji <erdeljimarko@gmail.com>
2023-11-16 18:04:36 +0100

    fix: Fixed an issue where swagger wasn't showing properly

--- backend/SmartHome.WebApi/SmartHome.WebApi/Program.cs
+++ backend/SmartHome.WebApi/SmartHome.WebApi/Program.cs
@@ -64,11 +64,7 @@ var app = builder.Build();
 
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
-    c.RoutePrefix = string.Empty;
-});
+app.UseSwaggerUI();
 
 
 app.UseCors("AllowAll");
