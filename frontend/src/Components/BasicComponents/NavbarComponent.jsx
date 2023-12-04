6bffa63b7496306fbccb0e6f468b0ee8773b12e3
Ognjen Radovanovic <ognjen34car@gmail.com>
2023-12-04 21:17:31 +0100

    styled navbar fix

--- frontend/src/Components/BasicComponents/NavbarComponent.jsx
+++ frontend/src/Components/BasicComponents/NavbarComponent.jsx
@@ -1,57 +1,69 @@
 import React from "react";
 import "./navbar.css";
-import {Link, useNavigate} from "react-router-dom"
-import { Button } from "@mui/material";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Container } from "@mui/material";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import BasicButton from "./BasicButton";
 import { logout } from "../Services/UserService";
 
-const NavbarComponent = ({loggedUser}) => {
-    const navigate = useNavigate();
-    const handleLogout = async ()=>{
-        const response = await logout();
-        navigate("/")
-        window.location.reload();
-    }
+const NavbarComponent = ({ loggedUser }) => {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    const response = await logout();
+    navigate("/");
+    window.location.reload();
+  };
 
-    return (
-        <div className="navbar-component">
-            <HomeOutlinedIcon  sx={{ fontSize: 50, color: "white" }}> </HomeOutlinedIcon>
-            <div className="menu-container">
-                {loggedUser != null && (
-                    <>
-                        <Link to = {'properties'}>
-                            <BasicButton color={'secondary'}    text={"Properties"} variant="outlined"></BasicButton>
-                        </Link>
-                        <Link>
-                            <BasicButton color={'secondary'}   text={"Button1"} variant="outlined"></BasicButton>
-                        </Link>
-                        {loggedUser.role == 2 &&
-                        <Link to={'admins'}>
-                            <BasicButton  color={'secondary'}   text={"Admins"} variant="outlined"></BasicButton>
-                        </Link>}
-                    </>)}
-            </div>
-            <div className="menu-container">
-                {loggedUser != null ? 
-                (
-                    <>
-                        <p>{loggedUser['name']}</p>
-                        <BasicButton color={'secondary'}  text={"Logout"} onClick={handleLogout} variant={"outlined"}></BasicButton>
-                    </>
-                ) :
-                (
-                    <>
-                    <Link to={'/login'}>
-                        <BasicButton color={'secondary'}  text={ "Login"} variant="contained"></BasicButton>
-                    </Link>
-                    <Link to={'/register'}>
-                        <BasicButton color={'secondary'}  text={"Register"} variant={"outlined"}></BasicButton>
-                    </Link>
-                </>)}
-            </div>
-        </div>
-    );
+  return (
+    <div className="navbar-component">
+      <div className="menu-container">
+        {loggedUser != null && (
+          <>
+            <Link className="nav-link nav-btn nav-first" to={"properties"}>
+              <div>Properties</div>
+            </Link>
+            <Link className="nav-link nav-btn" to={"properties"}>
+              <div>Butoon</div>
+            </Link>
+            {loggedUser.role == 2 && (
+              <Link className="nav-link nav-btn" to={"admins"}>
+              <div>Admins</div>
+            </Link>
+            )}
+          </>
+        )}
+      </div>
+      <div className="menu-container">
+        {loggedUser != null ? (
+          <>
+            <p
+            className="logout nav-btn"
+              onClick={handleLogout}
+            >
+              Logout
+            </p>
+          </>
+        ) : (
+          <>
+            <Link to={"/login"}>
+              <BasicButton
+                color={"secondary"}
+                text={"Login"}
+                variant="contained"
+              ></BasicButton>
+            </Link>
+            <Link to={"/register"}>
+              <BasicButton
+                color={"secondary"}
+                text={"Register"}
+                variant={"outlined"}
+              ></BasicButton>
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NavbarComponent;
