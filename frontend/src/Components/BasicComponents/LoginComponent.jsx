c86724b79a2cb7f8a301f2f4b78e1fdd452382ac
rozicd <dusan_rozic@hotmail.co.uk>
2024-02-03 22:46:28 +0100

    style:added dialog instaod of alerts for login and register

--- frontend/src/Components/BasicComponents/LoginComponent.jsx
+++ frontend/src/Components/BasicComponents/LoginComponent.jsx
@@ -4,6 +4,13 @@ import HeaderComponent from "./HeaderComponent";
 import { useState, useEffect } from "react";
 import { signIn } from "../Services/UserService";
 import BasicForm from "./BasicForm";
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+
+
 const loginTemplate = [
     
     {
@@ -23,41 +30,58 @@ const loginTemplate = [
 ]
 
 const LoginComponent = () => {
-    const [userCredentials, setUserCredentials] = useState({})
+    const [userCredentials, setUserCredentials] = useState({});
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogTitle, setDialogTitle] = useState("");
+    const [dialogContent, setDialogContent] = useState("");
 
     useEffect(() => {
-
         console.log("Updated userCredentials:", userCredentials);
-      }, [userCredentials]);
+    }, [userCredentials]);
 
-    const handleSubmit = async (state) =>{
-        setUserCredentials(state)
-        console.log(userCredentials)
-        try{
+    const handleCloseDialog = () => {
+        setDialogOpen(false);
+    };
+
+    const handleSubmit = async (state) => {
+        setUserCredentials(state);
+        console.log(userCredentials);
+        try {
             const response = await signIn(state);
-        if(response){
-            window.location.reload();
+            if (response) {
+                window.location.reload();
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 500) {
+                setDialogTitle("Account Not Activated");
+                setDialogContent("Account not activated! Check your email.");
+                setDialogOpen(true);
+            } else {
+                setDialogTitle("Error");
+                setDialogContent(error.response?.data || "Problem occurred");
+                setDialogOpen(true);
+            }
         }
-        }catch(error){
-            if(error.response.status == 500){
-                console.log("usao")
-                window.alert("Account not activated! Check your email.")
-            }else{
-                window.alert(error.response.data || "Problem occured")
-            } 
-        }
-        
-        
-      }
+    };
 
     return (
         <div className="login-component">
-            <HeaderComponent name = {"LOGIN"}></HeaderComponent>
+            <HeaderComponent name={"LOGIN"}></HeaderComponent>
             <div className="login-form">
-                <BasicForm template={loginTemplate} callback={handleSubmit} /> 
-                <p>Don't have account? <a href="/register">Sign up Here.</a></p> 
+                <BasicForm template={loginTemplate} callback={handleSubmit} />
+                <p>
+                    Don't have an account? <a href="/register">Sign up Here.</a>
+                </p>
             </div>
-            
+            <Dialog open={dialogOpen} onClose={handleCloseDialog}>
+                <DialogTitle>{dialogTitle}</DialogTitle>
+                <DialogContent>{dialogContent}</DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
